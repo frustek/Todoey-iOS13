864d18eeebe515e8676269ffcae98b2dfd43386f
Adam Mroz <MrozAdam1992@gmail.com>
2024-01-06 13:45:56 +0100

    Selection and deselection add

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -28,5 +28,19 @@ class TodoListViewController: UITableViewController {
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
 }
 
