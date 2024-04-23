4d5848545395ff835f5a28cc0029c5992472e0bf
Adam Mroz <mrozadam1992@gmail.com>
2024-04-23 11:16:05 +0200

    Add Category View, Controller  and Core Data

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Adam Mroz on 23/04/2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    // MARK: IBAction section
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    // MARK: TableView Datasource Methods
+    
+    // MARK: TableView Delegate Methods
+    
+    // MARK: TableView Manipulation Methods
+}
